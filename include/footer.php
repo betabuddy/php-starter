8498b4e24b959a820e59faa2f5041c53d80ce716
cravelight <cravelight@gmail.com>
2014-07-24 23:54:43 -0500

    initial commit

---
+++ include/footer.php
@@ -0,0 +1,10 @@
+    <div class="footer">
+        <div class="container">
+            <p class="text-muted">&COPY; Awesome Apps </p>
+        </div>
+    </div>    
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/assets/favicon.ico">
+
+    <title>Awesome App</title>
+
+    <!-- Bootstrap core CSS 
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+    -->
+    <!-- Using Bootswatch Flatly instead -->
+    <link href="//maxcdn.bootstrapcdn.com/bootswatch/3.2.0/flatly/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- Using Font Awesome because *Everything Is Awesome* -->
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link href="/assets/site.css" rel="stylesheet">
+</head>
+<body>
+<?php
+
+include './include/navbar.php';
+
+//eof    
